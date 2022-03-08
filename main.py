1ff218dc6e5f800d9eb185ce183412cc82157607
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2022-03-08 15:19:00 -0500

    terminal program

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from mimetypes import guess_all_extensions
+from turtle import back
 from puzzle import Puzzle
 from dictionary import Dictionary
 from game import Game
-from gui import Gui
+from colorama import Fore, Back, Style
+# from gui import MainWindow
+from colorama import init
+
+from puzzle import HintType
+
+
+def colored(text, color='', background='',):
+    render = ''
+    if color:
+        render += getattr(Fore, color.upper())
+    if background:
+        render += getattr(Back, background.upper())
+    render += text
+    render += Style.RESET_ALL
+    return render
 
 
 game = Game(
@@ -10,5 +26,24 @@ game = Game(
     puzzle=Puzzle(solution="perch")
 )
 
-gui = Gui(game=game)
-gui.run()
+init()
+round = 0
+while not game.has_won():
+    round += 1
+    guess = input(f'Guess {round}: ')
+    game.guess(word=guess)
+    try:
+        hints = game.hints[guess]
+    except KeyError:
+        round -= 1
+        continue
+    
+    colors = {
+        HintType.CORRECT: ('black', 'green'),
+        HintType.WRONG_PLACE: ('white', 'red'),
+        HintType.NOT_PRESENT: ('black', 'white'),
+    }
+    for letter, hint in hints:
+        color = colors[hint]
+        print(colored(letter, *color), end='')
+    print('')
