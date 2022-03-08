80812c8884be347711d8b9292c60878775610801
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2022-03-08 17:20:55 -0500

    new version

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='wordguess',
+    version='0.0.1'
     description='A Wordle terminal clone in Python',
     packages=['wordguess'],
     package_data={'wordlists': ['data/words.txt']},
